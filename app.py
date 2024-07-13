5e2670086e8f7c39cea7ac10bd9e21db6930c4f7
Dephinate <varuns.india@gmail.com>
2024-07-12 20:45:07 -0700

    Add fineutne api request

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+import sys
+from fastapi.responses import Response
+from starlette.responses import RedirectResponse
+from src.api.schemas import TrainingRequest
+from src.api.schemas import TrainingConfig
+from src.features.finetune.finetune import FineTune
+
+# Place holder text and api object
+app = FastAPI()
+
+@app.post("/finetune")
+async def predict_route(request: TrainingRequest):
+    try:
+        request_data = request
+        if not request_data.config:
+            request_data.config = TrainingConfig()
+
+        finetune_pipe = FineTune(fine_tune_config = request_data)
+
+    except Exception as e:
+        raise e
+
+if __name__ == "__main__":
+    import app
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
